047099057745d40acbf7959efc35256a0b127971
gavar <max.stankevich.me@gmail.com>
2019-10-24 22:12:43 +0300

    chore: release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,15 @@
+/** @type {WsConfiguration} */
 const config = {
   ...require("@wrench/semantic-release-ws-preset-nodejs/default"),
 };
+
 config.workspace = {
   ...config.workspace,
+  reduceReleaseType: "patch",
 };
+
 config.packages = {
   ...config.packages,
 };
+
 module.exports = config;
